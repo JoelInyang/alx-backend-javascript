4b713c84d4573e53243722d329394a20b728838f
JoelInyang <joelinyang2003@gmail.com>
2023-04-21 08:15:25 +0100

    ES6

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,7 @@
+//Using the prototype return a promise
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) resolve({ status: 200, body: 'Success' });
+    else reject(new Error('The fake API is not working currently'));
+  });
+}
---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+//Append three handlers to the function according to the prototype.
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
